bb4489c7ae1bda9b6dce6247c032e4da3890a75e
Cade Warbington <cadew@Cades-MacBook-Air.local>
2025-08-27 10:08:50 -0500

    merged main and removed duplicates

--- src/lib/features/todo.jsx
+++ src/lib/features/todo.jsx
@@ -1,20 +1,27 @@
-import { createReducer } from "@reduxjs/toolkit"
+import { createReducer } from "@reduxjs/toolkit";
 
-const todosReducer = createReducer([{task: "thing1", id: 1}, {task: "thing2", id: 2}, {task: "thing3", id: 3}], (builder) => {
+const todosReducer = createReducer(
+  [
+    { task: "thing1", id: 1 },
+    { task: "thing2", id: 2 },
+    { task: "thing3", id: 3 },
+  ],
+  (builder) => {
     builder
-      .addCase('ADD_TODO', (state, action) => {
+      .addCase("ADD_TODO", (state, action) => {
         // "mutate" the array by calling push()
-        state.push(action.payload)
+        state.push(action.payload);
       })
-      .addCase('TOGGLE_TODO', (state, action) => {
-        const todo = state[action.payload.index]
+      .addCase("TOGGLE_TODO", (state, action) => {
+        const todo = state[action.payload.index];
         // "mutate" the object by overwriting a field
-        todo.completed = !todo.completed
+        todo.completed = !todo.completed;
       })
-      .addCase('REMOVE_TODO', (state, action) => {
+      .addCase("REMOVE_TODO", (state, action) => {
         // Can still return an immutably-updated value if we want to
-        return state.filter((todo, i) => i !== action.payload.index)
-      })
-  })
+        return state.filter((todo, i) => i !== action.payload.index);
+      });
+  }
+);
 
-export default todosReducer
+export default todosReducer;
