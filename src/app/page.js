b1f11b3e31c399927b316720d7ac43848a655640
Cade Warbington <cadew@Cades-MacBook-Air.local>
2025-09-04 17:14:46 -0500

    added coin table

--- src/app/page.js
+++ src/app/page.js
@@ -43,11 +43,29 @@ export default function Home() {
     <StoreProvider>
       <List />
       <main className="m-[20px]">
-        <div>
-          {isLoading
-            ? "fetching data..."
-            : coinList.map((item) => <h1 key={item.id}>{item.id}</h1>)}
-        </div>
+        <p>{isLoading ? "Fetching data..." : ""}</p>
+        {coinList.map((item) => (
+          <div key={item.id} className="flex gap-[20px]">
+            <img src={item.image} className="w-[30px]"></img>
+            <h1>{`${item.name}[${item.symbol.toUpperCase()}]`}</h1>
+            <p>{item.current_price}</p>
+            <p>
+              {item.price_change_percentage_1h_in_currency
+                ? `${item.price_change_percentage_1h_in_currency.toFixed(2)}%`
+                : "null"}
+            </p>
+            <p>
+              {item.price_change_percentage_24h_in_currency
+                ? `${item.price_change_percentage_24h_in_currency.toFixed(2)}%`
+                : "null"}
+            </p>
+            <p>
+              {item.price_change_percentage_7d_in_currency
+                ? `${item.price_change_percentage_7d_in_currency.toFixed(2)}%`
+                : "null"}
+            </p>
+          </div>
+        ))}
         <p>{hasError ? "ERROR" : ""}</p>
       </main>
     </StoreProvider>
