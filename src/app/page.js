2c6b703868fe607d380b344f551554081c3066d1
Cade Warbington <cadew@Cades-MacBook-Air.local>
2025-09-11 16:26:03 -0500

    made the coin table and coin percentages their own components so the code is less cluttered

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,8 @@ import StoreProvider from "./StoreProvider";
 import { useAppSelector } from "@/lib/hooks";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Progress } from "@/components/ui/progress";
 
-import UpIcon from "../assets/UpIcon.svg";
-import DownIcon from "../assets/DownIcon.svg";
+import CoinTable from "@/components/coinTable/coinTable";
 
 const List = () => {
   const todos = useAppSelector((state) => state.todos);
@@ -56,91 +54,7 @@ export default function Home() {
           <p>24h%</p>
           <p>7d%</p>
         </div>
-        <div className="flex flex-col gap-[8px]">
-          {coinList.map((item, index) => (
-            <div
-              key={item.id}
-              className="flex gap-[20px] bg-[#191925] p-[20px] rounded-md items-center"
-            >
-              <p>{index + 1}</p>
-              <div className="flex items-center gap-[16px] w-[208px] flex-wrap">
-                <img src={item.image} className="w-[32px]"></img>
-                <h1 className="align-baseline">{`${
-                  item.name
-                }[${item.symbol.toUpperCase()}]`}</h1>
-              </div>
-              <p>{`$${item.current_price.toFixed(2)}`}</p>
-              <div className="flex items-center gap-[4px]">
-                {item.price_change_percentage_1h_in_currency >= 0 ? (
-                  <UpIcon />
-                ) : (
-                  <DownIcon />
-                )}
-                <p
-                  className={
-                    item.price_change_percentage_1h_in_currency >= 0
-                      ? "text-[#01F1E3]"
-                      : "text-[#FE2264]"
-                  }
-                >
-                  {item.price_change_percentage_1h_in_currency
-                    ? `${item.price_change_percentage_1h_in_currency.toFixed(
-                        2
-                      )}%`
-                    : "null"}
-                </p>
-              </div>
-              <div className="flex items-center gap-[4px]">
-                {item.price_change_percentage_24h_in_currency >= 0 ? (
-                  <UpIcon />
-                ) : (
-                  <DownIcon />
-                )}
-                <p
-                  className={
-                    item.price_change_percentage_24h_in_currency >= 0
-                      ? "text-[#01F1E3]"
-                      : "text-[#FE2264]"
-                  }
-                >
-                  {item.price_change_percentage_24h_in_currency
-                    ? `${item.price_change_percentage_24h_in_currency.toFixed(
-                        2
-                      )}%`
-                    : "null"}
-                </p>
-              </div>
-              <div className="flex items-center gap-[4px]">
-                {item.price_change_percentage_7d_in_currency >= 0 ? (
-                  <UpIcon />
-                ) : (
-                  <DownIcon />
-                )}
-                <p
-                  className={
-                    item.price_change_percentage_7d_in_currency >= 0
-                      ? "text-[#01F1E3]"
-                      : "text-[#FE2264]"
-                  }
-                >
-                  {item.price_change_percentage_7d_in_currency
-                    ? `${item.price_change_percentage_7d_in_currency.toFixed(
-                        2
-                      )}%`
-                    : "null"}
-                </p>
-              </div>
-              <Progress
-                value={(item.total_volume / item.market_cap) * 100}
-                className="w-[228px] h-[6px]"
-              />
-              <Progress
-                value={(item.circulating_supply / item.total_supply) * 100}
-                className="w-[228px] h-[6px]"
-              />
-            </div>
-          ))}
-        </div>
+        <CoinTable coinList={coinList} />
         <p>{hasError ? "ERROR" : ""}</p>
       </main>
     </StoreProvider>
---
+++ src/components/coinTable/CoinTable.jsx
@@ -0,0 +1,43 @@
+import { Progress } from "@/components/ui/progress";
+import CoinPercentage from "./CoinPercentage";
+
+const CoinTable = ({ coinList }) => {
+  return (
+    <div className="flex flex-col gap-[8px]">
+      {coinList.map((item, index) => {
+        const priceChange1h = item.price_change_percentage_1h_in_currency;
+        const priceChange24h = item.price_change_percentage_24h_in_currency;
+        const priceChange7d = item.price_change_percentage_7d_in_currency;
+
+        return (
+          <div
+            key={item.id}
+            className="flex gap-[20px] bg-[#191925] p-[20px] rounded-md items-center"
+          >
+            <p>{index + 1}</p>
+            <div className="flex items-center gap-[16px] w-[208px] flex-wrap">
+              <img src={item.image} className="w-[32px]"></img>
+              <h1 className="align-baseline">{`${
+                item.name
+              }[${item.symbol.toUpperCase()}]`}</h1>
+            </div>
+            <p>{`$${item.current_price.toFixed(2)}`}</p>
+            <CoinPercentage price_percentage={priceChange1h} />
+            <CoinPercentage price_percentage={priceChange24h} />
+            <CoinPercentage price_percentage={priceChange7d} />
+            <Progress
+              value={(item.total_volume / item.market_cap) * 100}
+              className="w-[228px] h-[6px]"
+            />
+            <Progress
+              value={(item.circulating_supply / item.total_supply) * 100}
+              className="w-[228px] h-[6px]"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CoinTable;
